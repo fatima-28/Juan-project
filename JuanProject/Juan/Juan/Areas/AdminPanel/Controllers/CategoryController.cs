4af97ed5f2a4814b8ba9363f6aa79f3eb4deea8c
fatima-28 <2n6gddb@code.edu.az>
2022-06-12 02:18:08 -0700

    Model binding, admin panel view

---
+++ JuanProject/Juan/Juan/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Juan.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.DAL;
+
+namespace Juan.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class CategoryController : Controller
+    {
+        public AppDbContext _context { get;  }
+        private IEnumerable<Category> categories;
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+            categories = _context.Categories.Where(c => !c.IsDeleted);
+        }
+        public IActionResult Index()
+        {
+            return View(categories);
+        }
+    }
+}
--- JuanProject/Juan/Juan/Areas/AdminPanel/Controllers/DashboardController.cs
+++ JuanProject/Juan/Juan/Areas/AdminPanel/Controllers/DashboardController.cs
@@ -3,12 +3,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApp.DAL;
 
 namespace Juan.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
+
+  
     public class DashboardController : Controller
     {
+        public AppDbContext _context { get; set; }
+        public DashboardController(AppDbContext context)
+        {
+            _context = context;
+
+        }
         public IActionResult Index()
         {
             return View();
