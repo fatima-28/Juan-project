3c0aaa7bbf112b58dc375a44ce72e1ef95c1fa0e
fatima-28 <2n6gddb@code.edu.az>
2022-06-12 19:41:38 -0700

    Slider admin panel view created

---
+++ JuanProject/Juan/Juan/Areas/AdminPanel/Controllers/SliderController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.DAL;
+
+namespace Juan.Areas.AdminPanel.Controllers
+{
+    [Area("AdminPanel")]
+    public class SliderController : Controller
+    {
+        private AppDbContext _context { get; }
+        private IWebHostEnvironment _env { get; }
+        public SliderController(AppDbContext context, IWebHostEnvironment env)
+        {
+
+            _context = context;
+            _env = env;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Slides);
+        }
+    }
+}
