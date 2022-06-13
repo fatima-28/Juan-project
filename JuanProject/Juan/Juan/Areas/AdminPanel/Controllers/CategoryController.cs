8f2edd08723cca7ee26ca5ea7aa95bc76b01fa49
fatima-28 <2n6gddb@code.edu.az>
2022-06-12 18:50:59 -0700

    update

--- JuanProject/Juan/Juan/Areas/AdminPanel/Controllers/CategoryController.cs
+++ JuanProject/Juan/Juan/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -79,5 +79,48 @@ namespace Juan.Areas.AdminPanel.Controllers
             return RedirectToAction(nameof(Index));
 
         }
+
+        public IActionResult Update(int? id)
+        {
+            if (id==null)
+            {
+                return BadRequest();
+            }
+            Category category = _context.Categories.Where(c => !c.IsDeleted).FirstOrDefault(c => c.Id == id);
+            if (category==null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Update(int? id, Category category)
+        {
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            Category Dbcategory = _context.Categories.Where(c => !c.IsDeleted).FirstOrDefault(c => c.Id == id);
+            if (Dbcategory == null)
+            {
+                return NotFound();
+            }
+
+            if (category.Name.ToLower() ==  Dbcategory.Name.ToLower()) { 
+            return RedirectToAction(nameof(Index));
+            }
+
+            bool IsExist = categories.Where(c => !c.IsDeleted).Any(c => c.Name.ToLower() == category.Name.ToLower());
+            if (IsExist)
+            {
+                ModelState.AddModelError("Name", $"{category.Name} is already exist!");
+                return View();
+            }
+            Dbcategory.Name = category.Name;
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+           
+        }
     }
 }
